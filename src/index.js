0ac90926587bb1b9e28305b1ebdb5db039f17278
Laurent De Smet and Tim De Pauw <pair+laurent.desmet+tim.depauw@zentrick.com>
2016-03-04 17:16:54 +0100

    Fix dependency tree construction

--- src/index.js
+++ src/index.js
@@ -4,18 +4,25 @@ export default (pkgRoot, options) => new Promise((resolve, reject) => {
   const result = { root: null, packages: {} }
   const walker = new TreeWalker(pkgRoot, options)
   walker.on('data', (pkg) => {
-    pkg.dependencies = {}
     if (pkg.parent != null) {
       const key = `${pkg.name}@${pkg.version}`
       if (result.packages[key] == null) {
         result.packages[key] = pkg
+        pkg.dependencies = {}
       }
-      result.packages[key].parent.dependencies[pkg.name] = pkg.version
+      const parentKey = `${pkg.parent.name}@${pkg.parent.version}`
+      const parent = result.packages[parentKey] || result.root
+      parent.dependencies[pkg.name] = pkg.version
     } else {
+      pkg.dependencies = {}
       result.root = pkg
     }
   })
-  walker.once('end', () => resolve(result))
-  walker.once('error', (err) => reject(err))
+  walker.once('end', () => {
+    resolve(result)
+  })
+  walker.once('error', (err) => {
+    reject(err)
+  })
   walker.run()
 })
